98c18c1555ef908622ea883498a96586c821f602
Joe Mann <contact@joemann.co.uk>
2019-11-12 22:22:48 +0100

    Small refactor.

--- mvt21/marshal.go
+++ mvt21/marshal.go
@@ -23,27 +23,118 @@ func Marshal(layers Layers, opts ...MarshalOption) ([]byte, error) {
 	for name, data := range layers {
 		layer := newLayer(string(name), data.Extent)
 
-		// marshal untyped metadata from layer
-		kvs, err := keyValues(data.Metadata)
-		if err != nil {
-			return nil, fmt.Errorf("failed to marshal metadata: %w", err)
+		if err := marshalKeyValues(data.Metadata, conf.metadata, layer); err != nil {
+			return nil, err
 		}
-
-		// then merge typed metadata from options
-		for _, metadata := range conf.metadata {
-			if _, ok := kvs[metadata.key]; ok {
-				return nil, fmt.Errorf("metadata with name '%s' already exists", metadata.key)
-			}
-			kvs[metadata.key] = metadata.value
-		}
-
-		setKeyValues(kvs, layer)
 
 		tile.Layers[i] = layer
 		i++
 	}
 
 	return proto.Marshal(&tile)
+}
+
+func marshalKeyValues(metadata geojson.PropertyList, additional []metadata, layer *spec.Tile_Layer) error {
+	// marshal untyped metadata from layer
+	kvs, err := keyValues(metadata)
+	if err != nil {
+		return fmt.Errorf("failed to marshal metadata: %w", err)
+	}
+
+	// then merge typed metadata from options
+	for _, metadata := range additional {
+		if _, ok := kvs[metadata.key]; ok {
+			return fmt.Errorf("metadata with name '%s' already exists", metadata.key)
+		}
+		kvs[metadata.key] = metadata.value
+	}
+
+	setKeyValues(kvs, layer)
+	return nil
+}
+
+// MarshalOption funcs can be used to modify the behaviour of Marshal.
+type MarshalOption func(*marshalConfig)
+
+// WithStringValue adds a typed string value to all layers.
+func WithStringValue(key, value string) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				StringValue: &value,
+			},
+		})
+	}
+}
+
+// WithFloat32Value adds a typed float32 value to all layers.
+func WithFloat32Value(key string, value float32) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				FloatValue: &value,
+			},
+		})
+	}
+}
+
+// WithFloat64Value adds a typed float64 value to all layers.
+func WithFloat64Value(key string, value float64) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				DoubleValue: &value,
+			},
+		})
+	}
+}
+
+// WithIntValue adds a typed int value to all layers.
+func WithIntValue(key string, value int64) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				IntValue: &value,
+			},
+		})
+	}
+}
+
+// WithUintValue adds a typed uint value to all layers.
+func WithUintValue(key string, value uint64) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				UintValue: &value,
+			},
+		})
+	}
+}
+
+// WithBoolValue adds a typed boolean value to all layers.
+func WithBoolValue(key string, value bool) MarshalOption {
+	return func(conf *marshalConfig) {
+		conf.metadata = append(conf.metadata, metadata{
+			key: key,
+			value: &spec.Tile_Value{
+				BoolValue: &value,
+			},
+		})
+	}
+}
+
+type marshalConfig struct {
+	metadata []metadata
+}
+
+type metadata struct {
+	key   string
+	value *spec.Tile_Value
 }
 
 func newLayer(name string, extent uint32) *spec.Tile_Layer {
@@ -130,88 +221,4 @@ func keyValues(metadata []geojson.Property) (map[string]*spec.Tile_Value, error)
 	}
 
 	return kvs, nil
-}
-
-// MarshalOption funcs can be used to modify the behaviour of Marshal.
-type MarshalOption func(*marshalConfig)
-
-// WithStringValue adds a typed string value to all layers.
-func WithStringValue(key, value string) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				StringValue: &value,
-			},
-		})
-	}
-}
-
-// WithFloat32Value adds a typed float32 value to all layers.
-func WithFloat32Value(key string, value float32) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				FloatValue: &value,
-			},
-		})
-	}
-}
-
-// WithFloat64Value adds a typed float64 value to all layers.
-func WithFloat64Value(key string, value float64) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				DoubleValue: &value,
-			},
-		})
-	}
-}
-
-// WithIntValue adds a typed int value to all layers.
-func WithIntValue(key string, value int64) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				IntValue: &value,
-			},
-		})
-	}
-}
-
-// WithUintValue adds a typed uint value to all layers.
-func WithUintValue(key string, value uint64) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				UintValue: &value,
-			},
-		})
-	}
-}
-
-// WithBoolValue adds a typed boolean value to all layers.
-func WithBoolValue(key string, value bool) MarshalOption {
-	return func(conf *marshalConfig) {
-		conf.metadata = append(conf.metadata, metadata{
-			key: key,
-			value: &spec.Tile_Value{
-				BoolValue: &value,
-			},
-		})
-	}
-}
-
-type marshalConfig struct {
-	metadata []metadata
-}
-
-type metadata struct {
-	key   string
-	value *spec.Tile_Value
 }
